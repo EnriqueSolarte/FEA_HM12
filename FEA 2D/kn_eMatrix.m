5cc5660941b847500a0c21147f2d8d2866825bfb
EnriqueSolarte <enrique.solarte.pardo@gmail.com>
2016-12-16 19:35:10 +0800

    ok

--- FEA 2D/kn_eMatrix.m
+++ FEA 2D/kn_eMatrix.m
@@ -1,26 +1,26 @@
-clc; clear all;
+function k=kn_eMatrix(mech,L,refined)
 
-h=0.01;
-v=0.25;
-E=200e9;
-L=10;
-refined=2;
+h=mech(1);
+v=mech(2);
+E=mech(3);
 
 y0=0;x0=0;
-k=zeros(8,8,refined^2);
+kn=zeros(8,8,refined^2);
 pitch=L/refined;
 index=1;
 for j=0:refined-1
-    y0=y0+j*pitch;
+    y0=j*pitch;
     
     for i=0:refined-1 
     
-        x0=x0+i*pitch;
+        x0=i*pitch;
         x1=x0+pitch;
         y1=y0+pitch;
         pts=[[x0 y0];[x1 y0];[x1 y1];[x0 y1]];
-        k(:,:,index)=k_eMatrix(pts,h,v,E)  
+        kn(:,:,index)=k_eMatrix(pts,h,v,E);  
         index=index+1;
     end 
-    x=0;
+    x0=0;
+    
 end
+k=kn
